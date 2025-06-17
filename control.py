d52fb89a94ef5699b67938c056e89fa0379c2150
malopuma <benjaminwaldmann@gmx.net>
2025-06-17 20:16:23 +0200

    Removed unused files

--- control.py
+++
@@ -1,9 +0,0 @@
-from task import Task
-
-class Control:
-
-    @staticmethod
-    def create_new_task():
-        new_task_title = input("Input new task: ")
-        task1 = Task(new_task_title)
-        print("Created new task: ", task1.title)
